e86c44c0d541a096828fd098ba4f603e079650e5
Raúl Antonio <raulantoniomb1994@gmail.com>
2020-07-26 15:53:37 -0400

    Vista de las páginas del diario personal, contiene un boton modal para agregar una nueva página.

---
+++ resources/views/diaries/pages_personal/button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ url('Nueva_pagina') }}" class="color-switcher-toggle animated pulse infinite">
+    <i class="material-icons">note_add</i>
+</a>
---
+++ resources/views/diaries/pages_personal/filters.blade.php
@@ -0,0 +1,110 @@
+
+{{-- title --}}
+<h6 class="main-sidebar__nav-title">Filtrar</h6>
+{{-- /title --}}
+
+{{-- content --}}
+<ul class="nav nav--no-borders flex-column">
+  <li class="nav-item">
+    <a class="nav-link" href="calendario.html">
+      <i class="material-icons">calendar_today</i>
+      <span>Fecha</span>
+    </a>
+  </li>
+  <li class="nav-item dropdown">
+    <a
+      class="nav-link dropdown-toggle"
+      data-toggle="dropdown"
+      href="#"
+      role="button"
+      aria-haspopup="false"
+      aria-expanded="false"
+    >
+      <i class="material-icons"></i>
+      <span>Categorias</span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-small">
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Amigos</a
+      >
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Amor</a
+      >
+
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Entretenimiento</a
+      >
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Negocio</a
+      >
+
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Que hacer</a
+      >
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Vacaciones</a
+      >
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Sin categoria(2)</a
+      >
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a
+      class="nav-link dropdown-toggle"
+      data-toggle="dropdown"
+      href="#"
+      role="button"
+      aria-haspopup="false"
+      aria-expanded="false"
+    >
+      <i class="material-icons">mood</i>
+      <span>Est. Ánimo</span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-small">
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Alegre(1)</a
+      >
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Eufórico</a
+      >
+
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Fatal</a
+      >
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Mal</a
+      >
+
+      <a class="dropdown-item" href="file-manager-cards.html"
+        >Normal</a
+      >
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Sin Est. Ánimo(1)</a
+      >
+    </div>
+  </li>
+
+  <li class="nav-item dropdown">
+    <a
+      class="nav-link dropdown-toggle"
+      data-toggle="dropdown"
+      href="#"
+      role="button"
+      aria-haspopup="false"
+      aria-expanded="false"
+    >
+      <i class="material-icons">room</i>
+      <span>Ubicacion</span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-small">
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Arturo prat 107, Linares</a
+      >
+      <a class="dropdown-item" href="file-manager-list.html"
+        >Sin Ubicacion</a
+      >
+    </div>
+  </li>
+</ul>
+{{-- /content --}}
