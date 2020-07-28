ae47af7f5297f45b9d60ccbeb6e4074c63b28801
Raúl Antonio <raulantoniomb1994@gmail.com>
2020-07-27 22:13:07 -0400

    Vista que permite ver la actividades de una meta, este puede ser objetivos terminados, agregados y eliminados.

---
+++ resources/views/app/objectives_and_goals/objectives/history.blade.php
@@ -0,0 +1,95 @@
+@extends('app.layouts.app')
+
+@section('title')
+    Historial
+@endsection
+
+@section('Objetivos_y_metas')
+    active
+@endsection
+
+@section('Historial_objetivos')
+    active
+@endsection
+
+
+@section('filters')
+    @include('app.objectives_and_goals.objectives.filters')
+@endsection
+
+
+@section('content')
+
+    <div class="main-content-container container-fluid px-4 pb-4">
+
+        {{-- title content header --}}
+        <div class="page-header row no-gutters py-4">
+            <div class="col">
+                <span class="text-uppercase page-subtitle">Objetivos y metas</span>
+                <h3 class="page-title">Historial</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card card-small user-activity mb-4">
+                    <div class="card-header border-bottom">
+                        <h6 class="m-0">Actividades</h6>
+                        <div class="block-handle"></div>
+                    </div>
+                    <div class="card-body p-0">
+
+                    </div>
+                    <div class="user-activity__item pr-3 py-3">
+                        <div class="user-activity__item__icon">
+                            <i class="material-icons"></i>
+                        </div>
+                        <div class="user-activity__item__content">
+                            <span class="text-light">hace 2 horas</span>
+                            <p>Marcaste <a href="#">1 tarea</a> como <span
+                                    class="badge badge-pill badge-outline-success">Completada</span>
+                        </div>
+                    </div>
+
+                    <div class="user-activity__item pr-3 py-3">
+                        <div class="user-activity__item__icon">
+                            <i class="material-icons"></i>
+                        </div>
+                        <div class="user-activity__item__content">
+                            <span class="text-light">2 Days ago</span>
+                            <p>Agregaste <a href="#">2 nuevas tareas</a> a la meta <a href="#">Aprender canción Chop Suey -
+                                    SOAD</a>:</p>
+                            <ul class="user-activity__item__task-list mt-2">
+                                <li>
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="user-activity-task-1">
+                                        <label class="custom-control-label" for="user-activity-task-1">solo final.</label>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="user-activity-task-2">
+                                        <label class="custom-control-label" for="user-activity-task-2">estribillo
+                                            final.</label>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="user-activity__item pr-3 py-3">
+                        <div class="user-activity__item__icon">
+                            <i class="material-icons"></i>
+                        </div>
+                        <div class="user-activity__item__content">
+                            <span class="text-light">hace 2 dias</span>
+                            <p>Existieron <a href="#">3 tareas</a> que fueron <span
+                                    class="badge badge-pill badge-outline-danger">Eliminadas</span>
+                                en la meta <a href="#">Aprender canción Chop Suey - SOAD</a> project.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
